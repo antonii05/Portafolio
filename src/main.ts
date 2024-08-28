2ab261ef64547ec102b5150689e71a9041cd916d
Antonio Blazquez <antblajim@gmail.com>
2024-08-28 20:25:58 +0200

    Develop -- Se instala y configura GSAP y PRETTIER

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,21 @@
-import './assets/main.css'
+import './assets/css/style.css'
 
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+import * as gsapPlugins from '@/dependencies/gsap-plugins'
+import { gsap } from 'gsap'
 
+// Importa los estilos de Bootstrap
+import 'bootstrap/dist/css/bootstrap.min.css'
+
+// Importa los scripts de Bootstrap
+import 'bootstrap/dist/js/bootstrap.bundle.min.js'
+
+gsap.registerPlugin(gsapPlugins)
 const app = createApp(App)
 
 app.use(router)
+app.use(gsap)
 
 app.mount('#app')
