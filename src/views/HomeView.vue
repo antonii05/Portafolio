7e18b34ce6d9b0929555a432a07d2982ec871f6c
antonio <antblajim@gmail.com>
2025-05-27 16:42:36 +0200

    DEVELOP - STYLE - SE AGREGA AL CENTRO EL TITULO

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,10 @@
 <template>
     <div class="home-container">
-        <div class="position-absolute top-0 start-0 w-100 h-100">
+        <div class="shapes-background">
             <Shapes />
         </div>
-        <div class="d-flex justify-content-center text-center">
+
+        <div class="header-centered">
             <h1 class="display-3 fw-bold text-white text-shadow mb-4">
                 Bienvenido a mi Portafolio
             </h1>
@@ -17,9 +18,28 @@ import Shapes from '@/components/Shapes.vue'
 
 <style scoped>
 .home-container {
-    min-height: 100vh;
     position: relative;
+    height: 100vh;
+    width: 100%;
 }
+
+.shapes-background {
+    position: absolute;
+    width: 100%;
+    height: 100%;
+    z-index: 0;
+}
+
+/* title */
+.header-centered {
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    z-index: 1;
+    text-align: center;
+}
+
 .text-shadow {
     text-shadow: 0 2px 8px rgba(0, 0, 0, 0.5);
 }
